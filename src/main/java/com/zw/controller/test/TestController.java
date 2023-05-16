feeabaac7ca027c3ded1619f9e36bd1431a1ddd5
zw <644988358@qq.com>
2023-05-16 11:48:09 +0800

    添加测试机器配置

--- src/main/java/com/zw/controller/test/TestController.java
+++ src/main/java/com/zw/controller/test/TestController.java
@@ -22,7 +22,7 @@ import java.util.concurrent.locks.Lock;
  * 总结：由于jdk8 windws下nio机制很拉跨，很可能是windws下创建1000以上线程放方式，加锁时会超时，
  * 解决办法，用线程池模式，或者用centos7.5
  * 注意：linux下创建50000线程去加锁，稳如狗，更不用说用线程池了
- * centsos 7.5测试结果
+ * centsos 7.5 4g 8线程 虚拟机(宿主机 dell g3 Intel(R)Core(TM) i5-9300H 32g内存 win10 64位专业版,512固态 )测试结果
  * 2023-05-16 11:34:30.518  INFO 105097 --- [io-19001-exec-1] com.zw.controller.test.TestController    : 线程累加值:50000:111839.0毫秒,111.84秒,1.86分钟 (注意这个是创建50000个线程进行加锁)
  * 2023-05-16 11:38:56.553  INFO 105097 --- [io-19001-exec-3] com.zw.controller.test.TestController    : 线程池大小：8线程累加值:50000:40671.0毫秒,40.67秒,0.68分钟
  * 2023-05-16 11:40:23.625  INFO 105097 --- [io-19001-exec-4] com.zw.controller.test.TestController    : 线程池大小：16线程累加值:50000:39903.0毫秒,39.9秒,0.67分钟
